58c789f3667d5495348a9e33db9955aaade25fc4
CristiPV <cristi.valentin29@gmail.com>
2020-05-16 18:24:04 +0200

    Add Tests for Service layer
    Add Tests for Service layer with the exception of Add, Update & Delete ( further learning required )

--- src/test/java/dev/hotdeals/motorhome/RVUseCaseTests.java
+++ src/test/java/dev/hotdeals/motorhome/RVUseCaseTests.java
@@ -23,7 +23,7 @@ public class RVUseCaseTests
     RVRepo rvRepo;
 
     @Test
-    public void rvRepoFetchAllTest() throws Exception
+    public void rvRepoFetchAllTest() throws Exception // Why we throw Exception ?
     {
         List<RV> rvList = rvRepo.fetchAll();
         assertThat(rvList).isNotNull();
@@ -31,11 +31,91 @@ public class RVUseCaseTests
 
     @Autowired
     RVService rvService;
-
+    //region RV Service
     @Test
-    public void rvService() throws Exception
+    public void rvServiceFetchAllTest() throws Exception
     {
         List<RV> rvList = rvService.fetchAll();
         assertThat(rvList).isNotNull();
     }
+
+    @Test
+    public void rvServiceFetchByIDTest() throws Exception
+    {
+        RV rv = rvService.fetchByID(3000);
+        assertThat(rv).isNotNull();
+    }
+
+    @Test
+    public void rvServiceFetchAvailableTest() throws Exception
+    {
+        List<RV>  rvList = rvService.fetchAvailable();
+        assertThat(rvList).isNotNull();
+    }
+
+    @Test
+    public void rvServiceFetchRentedTest() throws Exception
+    {
+        List<RV> rvList = rvService.fetchAvailable();
+        assertThat(rvList).isNotNull();
+    }
+
+    @Test
+    public void rvServiceFetchRequiresCleaningTest() throws Exception
+    {
+        List<RV> rvList = rvService.fetchRequiresCleaning();
+        assertThat(rvList).isNotNull();
+    }
+
+    @Test
+    public void rvServiceFetchRequiresMaintenanceTest() throws Exception
+    {
+        List<RV> rvList = rvService.fetchRequiresMaintenance();
+        assertThat(rvList).isNotNull();
+    }
+
+//    @Test
+//    public void rvServiceAddRVTest() throws Exception
+//    {
+//        RV rv = new RV();
+//        rv.setBrand("testBrand");
+//        rv.setModel("testModel");
+//        rv.setColor("testColor");
+//        rv.setRvType("testType");
+//        rv.setPrice(0);
+//        boolean rvAdded = rvService.addRV(rv);
+//        assertThat(rvAdded).isTrue();
+//    }
+
+    @Test
+    public void rvServiceSortByPriceTest() throws Exception
+    {
+        List<RV> rvList = rvService.sortByPrice();
+        assertThat(rvList).isNotNull();
+    }
+
+    @Test
+    public void rvServiceSearchByModelTest() throws Exception
+    {
+        List<RV> rvList = rvService.searchByModel("");
+        assertThat(rvList).isNotNull();
+    }
+
+    @Test
+    public void rvServiceSearchByBrandTest() throws Exception
+    {
+        // Search for an empty string -> the query will end up as :
+        List<RV> rvList = rvService.searchByBrand("");
+        assertThat(rvList).isNotNull();
+    }
+
+    @Test
+    public void rvServiceSearchByIDTest() throws Exception
+    {
+        // Search for cars that will contain '3' in their ID ( by convention all car ids will start with 3, so the
+        // search will always return something as long as the table has cars in it)
+        List<RV> rvList = rvService.searchByID(3);
+        assertThat(rvList).isNotNull();
+    }
+    //endregion
 }
