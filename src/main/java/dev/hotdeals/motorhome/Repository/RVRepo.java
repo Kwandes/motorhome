c076243a911570872f7cc350bb9e304989eaeb78
Alex <nyt604@gmail.com>
2020-05-17 17:30:37 +0200

    Add try/catch to required methods in RVRepo + add tests for them
    Organized test by subclasses

--- src/main/java/dev/hotdeals/motorhome/Repository/RVRepo.java
+++ src/main/java/dev/hotdeals/motorhome/Repository/RVRepo.java
@@ -3,6 +3,7 @@ package dev.hotdeals.motorhome.Repository;
 import dev.hotdeals.motorhome.Model.RV;
 import dev.hotdeals.motorhome.Model.RentalContract;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.ResultSetExtractor;
@@ -11,6 +12,7 @@ import org.springframework.stereotype.Repository;
 
 import java.beans.Statement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -22,28 +24,66 @@ public class RVRepo
 
     public RVRepo() {}
 
+    /*
+        public RV fetchByID(int recreationalVehicleID)
+    {
+        String query = "SELECT * FROM rv WHERE id = ?;";
+        RowMapper<RV> rvRowMapper = new BeanPropertyRowMapper<>(RV.class);
+        RV rvToReturn;
+        try
+        {
+            rvToReturn = template.queryForObject(query, rvRowMapper, recreationalVehicleID);
+        } catch (EmptyResultDataAccessException e)
+        {
+            System.out.println("Fetch By ID call for " + id + "has failed");
+            System.out.println(e);
+            rvToReturn = null;
+        }
+        return rvToReturn;
+    }
+     */
+
     public RV fetchByID(int recreationalVehicleID)
     {
         String query = "SELECT * FROM rv WHERE id = ?;";
         RowMapper<RV> rvRowMapper = new BeanPropertyRowMapper<>(RV.class);
-        RV rvToReturn= template.queryForObject(query, rvRowMapper, recreationalVehicleID);
+        RV rvToReturn;
+        try
+        {
+            rvToReturn= template.queryForObject(query, rvRowMapper, recreationalVehicleID);
+        } catch (EmptyResultDataAccessException e)
+        {
+            System.out.println("Failed to retrieve RV of ID " + recreationalVehicleID + ".");
+            System.out.println(e);
+            rvToReturn = null;
+        }
         return rvToReturn;
     }
 
     public List<RV> fetchAll()
     {
         String query = "SELECT * FROM rv;";
+        List<RV> listToReturn;
         RowMapper<RV> rvRowMapper = new BeanPropertyRowMapper<>(RV.class);
-        List<RV> listToReturn = template.query(query, rvRowMapper);
+        try
+        {
+            listToReturn = template.query(query, rvRowMapper);
+        } catch (EmptyResultDataAccessException e)
+        {
+            System.out.println("No RV's have been retrieved from the database");
+            System.out.println(e);
+            listToReturn = new ArrayList<RV>();
+        }
         return listToReturn;
     }
 
     public boolean addRV(RV recreationalVehicle)
     {
         String query = "INSERT INTO rv ( brand, model, color, rv_type, price ) VALUES ( ?, ?, ?, ?, ? )";
-        int rowsAffected = template.update(query, recreationalVehicle.getBrand(), recreationalVehicle.getModel(), recreationalVehicle.getColor(),
+        int rowsAffected;
+        rowsAffected = template.update(query, recreationalVehicle.getBrand(), recreationalVehicle.getModel(), recreationalVehicle.getColor(),
                            recreationalVehicle.getRvType(), recreationalVehicle.getPrice());
-        boolean status = rowsAffected > 0;
+        boolean status = rowsAffected > 0; //sets to TRUE if rows have been affected, sets to FALSE if no row has been affected
         return status;
     }
 
@@ -56,7 +96,7 @@ public class RVRepo
                 recreationalVehicle.getFuelStatus(), recreationalVehicle.getKmDriven(), recreationalVehicle.getRvType(), recreationalVehicle.getPrice(),
                 recreationalVehicle.getRequiresCleaning(), recreationalVehicle.getRequiresMaintenance(), recreationalVehicle.getRequiresFurtherService(),
                 recreationalVehicle.getIsRented());
-        boolean status = rowsAffected > 0;
+        boolean status = rowsAffected > 0; //sets to TRUE if rows have been affected, sets to FALSE if no row has been affected
         return status;
     }
 
@@ -64,7 +104,7 @@ public class RVRepo
     {
         String query = "DELETE FROM rv WHERE id = ?";
         int rowsAffected = template.update(query, recreationalVehicleID);
-        boolean status = rowsAffected > 0;
+        boolean status = rowsAffected > 0; //sets to TRUE if rows have been affected, sets to FALSE if no row has been affected
         return status;
     }
 
@@ -72,7 +112,16 @@ public class RVRepo
     {
         String query = "SELECT * FROM rv WHERE is_rented = 0 AND requires_cleaning = 0 AND requires_maintenance = 0 AND requires_further_service = 0";
         RowMapper<RV> rvRowMapper = new BeanPropertyRowMapper<>(RV.class);
-        List<RV> listToReturn = template.query(query, rvRowMapper);
+        List<RV> listToReturn;
+        try
+        {
+            listToReturn = template.query(query, rvRowMapper);
+        } catch (EmptyResultDataAccessException e)
+        {
+            System.out.println("Failed to retrieve any available RV's from the database");
+            System.out.println(e);
+            listToReturn = new ArrayList<RV>();
+        }
         return listToReturn;
     }
 
@@ -80,7 +129,16 @@ public class RVRepo
     {
         String query = "SELECT * FROM rv WHERE is_rented = 1";
         RowMapper<RV> rvRowMapper = new BeanPropertyRowMapper<>(RV.class);
-        List<RV> listToReturn = template.query(query, rvRowMapper);
+        List<RV> listToReturn;
+        try
+        {
+            listToReturn = template.query(query, rvRowMapper);
+        } catch (EmptyResultDataAccessException e)
+        {
+            System.out.println("No RV's have been retrieved from the database");
+            System.out.println(e);
+            listToReturn = new ArrayList<RV>();
+        }
         return listToReturn;
     }
 
@@ -88,7 +146,16 @@ public class RVRepo
     {
         String query = "SELECT * FROM rv WHERE requires_cleaning = 1";
         RowMapper<RV> rvRowMapper = new BeanPropertyRowMapper<>(RV.class);
-        List<RV> listToReturn = template.query(query, rvRowMapper);
+        List<RV> listToReturn;
+        try
+        {
+            listToReturn = template.query(query, rvRowMapper);
+        } catch (EmptyResultDataAccessException e)
+        {
+            System.out.println("No RV's have been retrieved from the database");
+            System.out.println(e);
+            listToReturn = new ArrayList<RV>();
+        }
         return listToReturn;
     }
 
@@ -96,7 +163,17 @@ public class RVRepo
     {
         String query = "SELECT * FROM rv WHERE requires_maintenance = 1 OR requires_further_service = 1";
         RowMapper<RV> rvRowMapper = new BeanPropertyRowMapper<>(RV.class);
-        List<RV> listToReturn = template.query(query, rvRowMapper);
+        List<RV> listToReturn;
+        try
+        {
+            listToReturn = template.query(query, rvRowMapper);
+        }
+        catch (EmptyResultDataAccessException e)
+        {
+            System.out.println("No RV's have been retrieved from the database");
+            System.out.println(e);
+            listToReturn = new ArrayList<RV>();
+        }
         return listToReturn;
     }
 
@@ -104,7 +181,17 @@ public class RVRepo
     {
         String query = "SELECT * FROM rv ORDER BY price DESC";
         RowMapper<RV> rvRowMapper = new BeanPropertyRowMapper<>(RV.class);
-        List<RV> listToReturn = template.query(query, rvRowMapper);
+        List<RV> listToReturn;
+        try
+        {
+            listToReturn = template.query(query, rvRowMapper);
+        }
+            catch (EmptyResultDataAccessException e)
+        {
+            System.out.println("No RV's have been retrieved from the database");
+            System.out.println(e);
+            listToReturn = new ArrayList<RV>();
+        }
         return listToReturn;
     }
 
@@ -112,7 +199,16 @@ public class RVRepo
     {
         String query = "SELECT * FROM rv WHERE model LIKE CONCAT('%', ?, '%')";
         RowMapper<RV> rvRowMapper = new BeanPropertyRowMapper<>(RV.class);
-        List<RV> listToReturn = template.query(query, rvRowMapper, model);
+        List<RV> listToReturn;
+        try
+        {
+            listToReturn = template.query(query, rvRowMapper, model);
+        } catch (EmptyResultDataAccessException e)
+        {
+            System.out.println("No RV's have been retrieved from the database");
+            System.out.println(e);
+            listToReturn = new ArrayList<RV>();
+        }
         return listToReturn;
     }
 
@@ -120,7 +216,16 @@ public class RVRepo
     {
         String query = "SELECT * FROM rv WHERE brand LIKE CONCAT('%', ?, '%')";
         RowMapper<RV> rvRowMapper = new BeanPropertyRowMapper<>(RV.class);
-        List<RV> listToReturn = template.query(query, rvRowMapper, brand);
+        List<RV> listToReturn;
+        try
+        {
+            listToReturn = template.query(query, rvRowMapper, brand);
+        } catch (EmptyResultDataAccessException e)
+        {
+            System.out.println("No RV's have been retrieved from the database");
+            System.out.println(e);
+            listToReturn = new ArrayList<RV>();
+        }
         return listToReturn;
     }
 
@@ -128,7 +233,16 @@ public class RVRepo
     {
         String query = "SELECT * FROM rv WHERE id LIKE CONCAT('%', ?, '%')";
         RowMapper<RV> rvRowMapper = new BeanPropertyRowMapper<>(RV.class);
-        List<RV> listToReturn = template.query(query, rvRowMapper, recreationalVehicleID);
+        List<RV> listToReturn;
+        try
+        {
+            listToReturn = template.query(query, rvRowMapper, recreationalVehicleID);
+        } catch (EmptyResultDataAccessException e)
+        {
+            System.out.println("No RV's have been retrieved from the database");
+            System.out.println(e);
+            listToReturn = new ArrayList<RV>();
+        }
         return listToReturn;
     }
 
--- src/test/java/dev/hotdeals/motorhome/RVUseCaseTests.java
+++ src/test/java/dev/hotdeals/motorhome/RVUseCaseTests.java
@@ -17,64 +17,138 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
 
+
 @SpringBootTest
 public class RVUseCaseTests
 {
-
-    @Autowired
-    RVRepo rvRepo;
-
-    @Test
-    public void rvRepoFetchAllTest() throws Exception // Why we throw Exception ?
+    @SpringBootTest
+    public static class RVRepoTests
     {
-        List<RV> rvList = rvRepo.fetchAll();
-        assertThat(rvList).isNotNull();
+        @Autowired
+        RVRepo rvRepo;
+
+        @Test
+        public void rvRepoFetchAllTest() throws Exception // Why we throw Exception ?
+        {
+            List<RV> rvList = rvRepo.fetchAll();
+            assertThat(rvList).isNotEmpty();
+        }
+
+        @Test
+        public void rvRepoFetchByIDTest() throws Exception
+        {
+            RV rv = rvRepo.fetchByID(3000);
+            assertThat(rv).isNotNull();
+        }
+
+        @Test
+        public void rvRepoFetchAvailableTest() throws Exception
+        {
+            List<RV>  rvList = rvRepo.fetchAvailable();
+            assertThat(rvList).isNotEmpty();
+        }
+
+        @Test
+        public void rvRepoFetchRentedTest() throws Exception
+        {
+            List<RV> rvList = rvRepo.fetchAvailable();
+            assertThat(rvList).isNotEmpty();
+        }
+
+        @Test
+        public void rvRepoFetchRequiresCleaningTest() throws Exception
+        {
+            List<RV> rvList = rvRepo.fetchRequiresCleaning();
+            assertThat(rvList).isNotEmpty();
+        }
+
+        @Test
+        public void rvRepoFetchRequiresMaintenanceTest() throws Exception
+        {
+            List<RV> rvList = rvRepo.fetchRequiresMaintenance();
+            assertThat(rvList).isNotEmpty();
+        }
+
+        @Test
+        public void rvRepoSortByPriceTest() throws Exception
+        {
+            List<RV> rvList = rvRepo.sortByPrice();
+            assertThat(rvList).isNotEmpty();
+        }
+
+        @Test
+        public void rvRepoSearchByModelTest() throws Exception
+        {
+            List<RV> rvList = rvRepo.searchByModel("");
+            assertThat(rvList).isNotEmpty();
+        }
+
+        @Test
+        public void rvRepoSearchByBrandTest() throws Exception
+        {
+            // Search for an empty string -> the query will end up as :
+            List<RV> rvList = rvRepo.searchByBrand("");
+            assertThat(rvList).isNotEmpty();
+        }
+
+        @Test
+        public void rvRepoSearchByIDTest() throws Exception
+        {
+            // Search for cars that will contain '3' in their ID ( by convention all car ids will start with 3, so the
+            // search will always return something as long as the table has cars in it)
+            List<RV> rvList = rvRepo.searchByID(3);
+            assertThat(rvList).isNotEmpty();
+        }
     }
 
-    @Autowired
-    RVService rvService;
     //region RV Service
-    @Test
-    public void rvServiceFetchAllTest() throws Exception
+    @SpringBootTest
+    public static class RVServiceTests
     {
-        List<RV> rvList = rvService.fetchAll();
-        assertThat(rvList).isNotNull();
-    }
+        @Autowired
+        RVService rvService;
 
-    @Test
-    public void rvServiceFetchByIDTest() throws Exception
-    {
-        RV rv = rvService.fetchByID(3000);
-        assertThat(rv).isNotNull();
-    }
+        @Test
+        public void rvServiceFetchAllTest() throws Exception
+        {
+            List<RV> rvList = rvService.fetchAll();
+            assertThat(rvList).isNotEmpty();
+        }
 
-    @Test
-    public void rvServiceFetchAvailableTest() throws Exception
-    {
-        List<RV>  rvList = rvService.fetchAvailable();
-        assertThat(rvList).isNotNull();
-    }
+        @Test
+        public void rvServiceFetchByIDTest() throws Exception
+        {
+            RV rv = rvService.fetchByID(3000);
+            assertThat(rv).isNotNull();
+        }
 
-    @Test
-    public void rvServiceFetchRentedTest() throws Exception
-    {
-        List<RV> rvList = rvService.fetchAvailable();
-        assertThat(rvList).isNotNull();
-    }
+        @Test
+        public void rvServiceFetchAvailableTest() throws Exception
+        {
+            List<RV>  rvList = rvService.fetchAvailable();
+            assertThat(rvList).isNotEmpty();
+        }
 
-    @Test
-    public void rvServiceFetchRequiresCleaningTest() throws Exception
-    {
-        List<RV> rvList = rvService.fetchRequiresCleaning();
-        assertThat(rvList).isNotNull();
-    }
+        @Test
+        public void rvServiceFetchRentedTest() throws Exception
+        {
+            List<RV> rvList = rvService.fetchAvailable();
+            assertThat(rvList).isNotEmpty();
+        }
 
-    @Test
-    public void rvServiceFetchRequiresMaintenanceTest() throws Exception
-    {
-        List<RV> rvList = rvService.fetchRequiresMaintenance();
-        assertThat(rvList).isNotNull();
-    }
+        @Test
+        public void rvServiceFetchRequiresCleaningTest() throws Exception
+        {
+            List<RV> rvList = rvService.fetchRequiresCleaning();
+            assertThat(rvList).isNotEmpty();
+        }
+
+        @Test
+        public void rvServiceFetchRequiresMaintenanceTest() throws Exception
+        {
+            List<RV> rvList = rvService.fetchRequiresMaintenance();
+            assertThat(rvList).isNotEmpty();
+        }
 
 //    @Test -- Currently not implemented due to lack of knowledge of proper implementation.
 //    public void rvServiceAddRVTest() throws Exception
@@ -89,35 +163,37 @@ public class RVUseCaseTests
 //        assertThat(rvAdded).isTrue();
 //    }
 
-    @Test
-    public void rvServiceSortByPriceTest() throws Exception
-    {
-        List<RV> rvList = rvService.sortByPrice();
-        assertThat(rvList).isNotNull();
-    }
+        @Test
+        public void rvServiceSortByPriceTest() throws Exception
+        {
+            List<RV> rvList = rvService.sortByPrice();
+            assertThat(rvList).isNotEmpty();
+        }
 
-    @Test
-    public void rvServiceSearchByModelTest() throws Exception
-    {
-        List<RV> rvList = rvService.searchByModel("");
-        assertThat(rvList).isNotNull();
-    }
+        @Test
+        public void rvServiceSearchByModelTest() throws Exception
+        {
+            List<RV> rvList = rvService.searchByModel("");
+            assertThat(rvList).isNotEmpty();
+        }
 
-    @Test
-    public void rvServiceSearchByBrandTest() throws Exception
-    {
-        // Search for an empty string -> the query will end up as :
-        List<RV> rvList = rvService.searchByBrand("");
-        assertThat(rvList).isNotNull();
-    }
+        @Test
+        public void rvServiceSearchByBrandTest() throws Exception
+        {
+            // Search for an empty string -> the query will end up as :
+            List<RV> rvList = rvService.searchByBrand("");
+            assertThat(rvList).isNotEmpty();
+        }
 
-    @Test
-    public void rvServiceSearchByIDTest() throws Exception
-    {
-        // Search for cars that will contain '3' in their ID ( by convention all car ids will start with 3, so the
-        // search will always return something as long as the table has cars in it)
-        List<RV> rvList = rvService.searchByID(3);
-        assertThat(rvList).isNotNull();
+        @Test
+        public void rvServiceSearchByIDTest() throws Exception
+        {
+            // Search for cars that will contain '3' in their ID ( by convention all car ids will start with 3, so the
+            // search will always return something as long as the table has cars in it)
+            List<RV> rvList = rvService.searchByID(3);
+            assertThat(rvList).isNotEmpty();
+        }
     }
     //endregion
+
 }
