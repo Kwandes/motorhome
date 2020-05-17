ac58137eec96e4105868f96a983734513544ed65
Teodor Jonasson <teodor_jonasson@hotmail.com>
2020-05-18 01:46:32 +0200

    Fixed bugs.
    Spring mapping for the different pages (GetMapping) in HomeController done.
    
    Created all html page files for various:
     - createNew.html
     - requiresCleaning.html
     - requiresMaintenance.html
     - etc.
    
    Created the buttons for the RV page, but no funtionality made yet. just css and links to other pages.
    
    Created the main pages entrance:
     - customers.html
     - rentals.html
     - rvExtras.html
     with linked CSS files to each of them (empty canvas tho)

--- src/main/java/dev/hotdeals/motorhome/Controller/HomeController.java
+++ src/main/java/dev/hotdeals/motorhome/Controller/HomeController.java
@@ -12,8 +12,50 @@ public class HomeController
         return "home/index";
     }
 
+    /*
+    SALESMAN PAGE
+    * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+    */
     @GetMapping({"/salesmanMainPage"})
-    public String salesmanMainPage() {
-        return "home/salesmanMainPage";
+    public String salesmanMainPage()
+    {
+        return "motorhome/salesmanMainPage";
     }
+
+    @GetMapping({"/salesmanMainPage/customers"})
+    public String salesmanMainPageCustomers()
+    {
+        return "motorhome/customers";
+    }
+
+    @GetMapping({"/salesmanMainPage/rentals"})
+    public String salesmanMainPageRentals()
+    {
+        return "motorhome/rentals";
+    }
+
+    @GetMapping({"/salesmanMainPage/rv"})
+    public String salesmanMainPageRv()
+    {
+        return "motorhome/rv";
+    }
+
+    @GetMapping({"/salesmanMainPage/rvExtras"})
+    public String salesmanMainPageRvExtras()
+    {
+        return "motorhome/rvExtras";
+    }
+
+    /*JUST FOR TESTING ###########################*/
+    @GetMapping({"/salesmanMainPage/rv/viewAll"})
+    public String salesmanMainPageRvViewAll()
+    {
+        return "motorhome/rv/viewAll";
+    }
+    /*JUST FOR TESTING ###########################*/
+
+    /*
+    * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+    * END SALESMAN PAGE
+    */
 }
