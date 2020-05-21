c07e4d22b3514810f39fdf55daec707368c0ab96
Jan <jan.bogoryja@gmail.com>
2020-05-21 19:21:06 +0200

    Refactor and tweak Customer frontend
    Fixed one minor bug: the input for address2 
    Tweak order of variables etc

--- src/main/java/dev/hotdeals/motorhome/Controller/CustomerController.java
+++ src/main/java/dev/hotdeals/motorhome/Controller/CustomerController.java
@@ -3,7 +3,6 @@ package dev.hotdeals.motorhome.Controller;
 import dev.hotdeals.motorhome.Model.Customer;
 import dev.hotdeals.motorhome.Service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,7 +10,6 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.context.request.WebRequest;
 
-import javax.validation.constraints.Null;
 import java.util.List;
 
 @Controller
