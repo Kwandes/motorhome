eea0bfa081adbf0e1151d2167060f6ce60a341aa
Alex <nyt604@gmail.com>
2020-05-21 12:38:44 +0200

    Add completed CustomerRepo and its tests
    all confirmed working

---
+++ src/test/java/dev/hotdeals/motorhome/CustomerUseCaseTests.java
@@ -0,0 +1,130 @@
+package dev.hotdeals.motorhome;
+
+
+import dev.hotdeals.motorhome.Model.Customer;
+import dev.hotdeals.motorhome.Repository.CustomerRepo;
+import net.minidev.json.JSONUtil;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.thymeleaf.engine.IterationStatusVar;
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.contentOf;
+
+@SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class CustomerUseCaseTests
+{
+    @Autowired
+    CustomerRepo customerRepo;
+
+    @Test
+    @DisplayName("Fetch customer object by ID from DB")
+    public void customerRepoFetchById()
+    {
+        //given - User Of ID 2000 exists
+        Customer customerTestObject = null;
+        int customerIdToRetrieve = 2000;
+        //when
+        customerTestObject = customerRepo.fetchById(customerIdToRetrieve);
+        //then
+        assertThat(customerTestObject).isNotNull();
+    }
+
+    @Test
+    @DisplayName("Fetch all customers from DB")
+    public void customerRepoFetchAll()
+    {
+        // given - the DB is not empty
+        List<Customer> customersTestList = null;
+        // when
+        customersTestList = customerRepo.fetchAll();
+        // then
+        assertThat(customersTestList).isNotNull();
+    }
+
+    @Test
+    @Order(2)
+    @DisplayName("Add a customer to the DB")
+    public void customerRepoAddCustomer()
+    {
+        // given
+        Customer customer = new Customer();
+        customer.setFirstName("TestName");
+        customer.setLastName("TestLastName");
+        customer.setCpr("999999-9999");
+        customer.setPhoneNumber("12345678");
+        customer.setEmail("testemail@test.mail");
+        customer.setBirthdate("1990-12-24");
+        customer.setAddress("test of address name");
+        customer.setAddress2("test of second address name");
+        customer.setGender("Test");
+        // when
+        boolean success = customerRepo.addCustomer(customer);
+        // then
+        assertThat(success).isTrue();
+    }
+
+    @Test
+    @Order(3)
+    @DisplayName("Change a customer's information from the DB")
+    public void customerRepoUpdateCustomer()
+    {
+        // given - the ID exist
+        String originalFirstName = "TestName";
+        String EditedFirstName ="NameTest";
+        Customer testCustomer = customerRepo.searchByName(originalFirstName).get(0);
+        testCustomer.setFirstName(EditedFirstName);
+        // when
+        boolean status = customerRepo.updateCustomer(testCustomer);
+        // then
+        assertThat(status).isTrue();
+    }
+
+
+    @Test
+    @Order(4)
+    @DisplayName("Delete customer from DB using Id")
+    public void customerRepoDeleteCustomerTest()
+    {
+        // given - the Customer exists
+        String customerNameToDelete = "NameTest"; //name used to search for the customer to delete
+        Customer testCustomerDelete = customerRepo.searchByName(customerNameToDelete).get(0); //the Customer we want to delete
+        int customerIdToDelete = testCustomerDelete.getId(); //getting the ID of the customer to delete
+        // when
+        boolean customerDeleteTest = customerRepo.deleteCustomer(customerIdToDelete);
+        // then
+        assertThat(customerDeleteTest).isTrue();
+    }
+
+    @Test
+    @DisplayName("Search for a customer by CPR - partial input is valid")
+    public void customerRepoSearchByCpr()
+    {
+        // given - Cpr 160428-1231 exists
+        String cpr = "160428-1231";
+        // when
+        List<Customer> customerList = customerRepo.searchByCpr(cpr);
+        // then
+        assertThat(customerList).isNotEmpty();
+    }
+
+    @Test
+    @Order(1)
+    @DisplayName("Search for a customer by name")
+    public void customerRepoSearchByNameTest()
+    {
+        // given - name exists
+        String name = "Adele";
+        // when
+        List<Customer> customersReturnedFromSearch= null;
+        customersReturnedFromSearch = customerRepo.searchByName("Adel");
+        // then
+        assertThat(customersReturnedFromSearch).isNotEmpty();
+    }
+}
