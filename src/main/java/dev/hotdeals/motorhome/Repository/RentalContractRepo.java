ee6bcef5f3850bff18851ee86221513be3fd10ca
Teodor Jonasson <teodor_jonasson@hotmail.com>
2020-05-22 12:52:08 +0200

    Started ContractRepo
    - created fetchAll
    - created fetchContractByID

--- src/main/java/dev/hotdeals/motorhome/Repository/RentalContractRepo.java
+++ src/main/java/dev/hotdeals/motorhome/Repository/RentalContractRepo.java
@@ -2,7 +2,10 @@ package dev.hotdeals.motorhome.Repository;
 
 import dev.hotdeals.motorhome.Model.RentalContract;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -14,16 +17,41 @@ public class RentalContractRepo
     @Autowired
     JdbcTemplate template;
 
-    public static RentalContract fetchContractByID(int contractID)
+    public RentalContract fetchContractByID(int contractID)
     {
-        RentalContract contractToReturn = new RentalContract();
-        return contractToReturn;
+        String query = "SELECT * FROM rental_contract;";
+        RowMapper<RentalContract> rw = new BeanPropertyRowMapper<>(RentalContract.class);
+        RentalContract rentalContract;
+        try
+        {
+            rentalContract = template.queryForObject(query, rw, contractID);
+        }
+        catch (EmptyResultDataAccessException e)
+        {
+            System.out.println("Failed to retrieve the contract from given ID : " + contractID + ".");
+            System.out.println(e);
+            return null;
+        }
+        return rentalContract;
     }
 
     public List<RentalContract> fetchAll()
     {
-        List<RentalContract> listToReturn = null;
-        return listToReturn;
+        String query = "SELECT * FROM rental_contract;";
+
+        RowMapper<RentalContract> rw = new BeanPropertyRowMapper<>(RentalContract.class);
+        List<RentalContract> rentalContractList;
+        try
+        {
+            rentalContractList = template.query(query, rw);
+        }
+        catch (EmptyResultDataAccessException e)
+        {
+            System.out.println("Failed to retrieve the contract list.");
+            System.out.println(e);
+            return null;
+        }
+        return rentalContractList;
     }
 
     public boolean addContract(RentalContract rentalContract)
