baef6f15b3ec805cc3a9b129e302ef9c690888ba
CristiPV <cristi.valentin29@gmail.com>
2020-05-29 20:48:56 +0200

    Add Create Cleaning Plan Functionality
    Add new button to rv/viewAll - Create Cleaning Plan
    Add html for the creation form of the Cleaning Plan
    Add html for the Cleaning Plan print-out
    
    Note : After the creation of a Cleaning Plan, all selected RVs will be considered clean ( requiresCleaning -> false )

--- src/main/java/dev/hotdeals/motorhome/Controller/RvController.java
+++ src/main/java/dev/hotdeals/motorhome/Controller/RvController.java
@@ -5,7 +5,9 @@
 
 package dev.hotdeals.motorhome.Controller;
 
+import dev.hotdeals.motorhome.Model.Employee;
 import dev.hotdeals.motorhome.Model.RV;
+import dev.hotdeals.motorhome.Service.EmployeeService;
 import dev.hotdeals.motorhome.Service.RVService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -15,6 +17,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.context.request.WebRequest;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -22,6 +25,8 @@ public class RvController
 {
     @Autowired
     RVService rvService;
+    @Autowired
+    EmployeeService employeeService;
 
     // default mapping, redirects to actual viewAll file
     @GetMapping({"/rv", "/rv/", "/rv/viewAll/"})
@@ -134,6 +139,54 @@ public class RvController
         return "redirect:/rv/viewAll";
     }
 
+    @GetMapping("/rv/createCleaningPlan")
+    public String createCleaningPlan( Model model )
+    {
+        List<Employee> employeeList = employeeService.searchByPosition("Cleaning Staff");
+        model.addAttribute("employeeList", employeeList);
+        List<RV> rvList = rvService.fetchRequiresCleaning();
+        model.addAttribute("rvList", rvList);
+
+        return "/rv/createCleaningPlan";
+    }
+
+    @PostMapping("/rv/printCleaningPlan")
+    public String printCleaningPlan (WebRequest wr, Model model)
+    {
+        // Fetch Cleaner
+        int cleanerID = checkID("employeeID", wr, "/rv/printCleaningPlan");
+        if (cleanerID == -1)
+        {
+            return "redirect:/rv/errorParameters";
+        }
+        Employee cleaner = employeeService.fetchByID(cleanerID);
+
+        // Fetch List of Selected RVs
+        List<RV> rvList = new ArrayList<>();
+        String[] selectedRVList = wr.getParameterValues("rvID");
+        try
+        {
+            for (int i = 0; i < selectedRVList.length; i ++)
+            {
+                int rvID = Integer.parseInt(selectedRVList[i]);
+                RV rv = rvService.fetchByID(rvID);
+                rv.setRequiresCleaning(false);
+                rvService.updateRV(rv);
+                rvList.add(rv);
+            }
+        } catch (NullPointerException | NumberFormatException e)
+        {
+            System.out.println("Failed to retrieve Selected RV List");
+            System.out.println(e);
+        }
+
+        model.addAttribute("cleaner", cleaner);
+        model.addAttribute("rvList", rvList);
+
+        return "/rv/cleaningPlan";
+    }
+
+
     @GetMapping("/rv/errorParameters")
     public String errorParameters()
     {
@@ -157,5 +210,19 @@ public class RvController
             model.addAttribute("rvList", rvList);
             return "rv/viewAll";
         }
+    }
+
+    public int checkID (String varName, WebRequest wr, String mapping)
+    {
+        int x;
+        try
+        {
+            x = Integer.parseInt(wr.getParameter(varName));
+        } catch (NullPointerException | NumberFormatException e)
+        {
+            System.out.println("Failed to get parameter '" + varName + "' from the model in " + mapping + " : " + e);
+            return -1;
+        }
+        return x;
     }
 }
